f6d9afd7e5115ad7d0f973a5dd330b2ffbbba9bc
HansolWorld <hs9269@gmail.com>
2023-04-22 16:33:31 +0900

    feat: edit ViewController - add header array - add section header management function

--- Onban/Onban/ViewController.swift
+++ Onban/Onban/ViewController.swift
@@ -9,8 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var headerMessage = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        makeHeaderMessage()
+    }
+    
+    func makeHeaderMessage() {
+        let main = "온 가족이 좋아하는 든든한 메인 요리"
+        let soup = "정성과 건강이 가득 담긴 국물 요리"
+        let side = "식탁을 풍성하게 하는 정갈한 밑반찬"
+        
+        headerMessage.append(main)
+        headerMessage.append(soup)
+        headerMessage.append(side)
     }
 }
 
@@ -27,4 +41,12 @@ extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
         return cell
     }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "header", for: indexPath) as! HeaderCollectionReusableView
+        
+        header.headerLabel.text = headerMessage[indexPath.section]
+        return header
+    }
 }
+
