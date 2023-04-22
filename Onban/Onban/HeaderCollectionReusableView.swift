1707af7898c6005cbc5d42daf585317ef25f5ce1
HansolWorld <hs9269@gmail.com>
2023-04-22 16:29:37 +0900

    style: desabled force_cast rule

---
+++ Onban/Onban/HeaderCollectionReusableView.swift
@@ -0,0 +1,12 @@
+//
+//  HeaderCollectionReusableView.swift
+//  Onban
+//
+//  Created by apple on 2023/04/22.
+//
+
+import UIKit
+
+class HeaderCollectionReusableView: UICollectionReusableView {
+        
+}
