d0cf8e67359718bcb0eca9e833392186b0ff3d1a
demiiann <tkim0156@gmail.com>
2023-04-22 15:41:45 +0900

    feat: add numberOfSections function
    - for header

--- Onban/Onban/ViewController.swift
+++ Onban/Onban/ViewController.swift
@@ -11,21 +11,20 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 }
 
 extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 3
+    }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 7
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-        
         return cell
     }
-    
-    
 }
