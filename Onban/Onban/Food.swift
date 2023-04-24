fa06626c9714dab81ddd7e9f7febeb1ef3d39d87
HansolWorld <hs9269@gmail.com>
2023-04-25 06:50:35 +0900

    feat: HeaderMessage 객체 추가

--- Onban/Onban/Food.swift
+++ Onban/Onban/Food.swift
@@ -29,3 +29,26 @@ struct FoodResponse: Codable {
     let statusCode: Int
     let body: [Food]
 }
+
+enum  HeaderMessages: CaseIterable {
+    case main
+    case soup
+    case side
+    
+    var kind: String {
+        String(describing: self)
+    }
+}
+
+extension HeaderMessages: CustomStringConvertible {
+    var description: String {
+        switch self {
+        case .main:
+            return "온 가족이 좋아하는 든든한 메인 요리"
+        case .soup:
+            return "정성과 건강이 가득 담긴 국물 요리"
+        case .side:
+            return "식탁을 풍성하게 하는 정갈한 밑반찬"
+        }
+    }
+}
