ea9113f872dfea8b3e0a12e48dd1032958c39eef
demiiann <tkim0156@gmail.com>
2023-04-25 07:40:46 +0900

    feat: FoodCollectionViewCell 파일 생성

---
+++ Onban/Onban/FoodCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  FoodCollectionViewCell.swift
+//  Onban
+//
+//  Created by ilim on 2023/04/25.
+//
+
+import UIKit
+
+class FoodCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var foodThumbnail: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var desc: UILabel!
+    @IBOutlet weak var normalPrice: UILabel!
+    
+    @IBOutlet weak var priceStack: UIStackView!
+    @IBOutlet weak var badge: UIStackView!
+}
