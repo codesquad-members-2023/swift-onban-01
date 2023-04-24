b331674a5a1ce689ebba4e5ed16e7e879d807de3
HansolWorld <hs9269@gmail.com>
2023-04-24 20:17:52 +0900

    feat: Food 구조체 추가

---
+++ Onban/Onban/Food.swift
@@ -0,0 +1,31 @@
+//
+//  Food.swift
+//  Onban
+//
+//  Created by ilim on 2023/04/24.
+//
+
+import Foundation
+
+struct Food: Codable {
+    let detailHash: String
+    let imageUrl: String
+    let title: String
+    let description: String
+    let normalPrice: String?
+    let salePrice: String
+    let badge: [String]?
+    
+    enum CodingKeys: String, CodingKey {
+        case title, description, badge
+        case imageUrl = "image"
+        case detailHash = "detail_hash"
+        case normalPrice = "n_price"
+        case salePrice = "s_price"
+    }
+}
+
+struct FoodResponse: Codable {
+    let statusCode: Int
+    let body: [Food]
+}
