1961c8dcea9e2f27ade63182971e3326c5f2382a
demiiann <tkim0156@gmail.com>
2023-04-25 20:59:01 +0900

    feat: 헤더 메시지 딕셔너리 배열 선언

---
+++ Onban/OnbanConfigure.swift
@@ -0,0 +1,22 @@
+//
+//  OnbanUrl.swift
+//  Onban
+//
+//  Created by ilim on 2023/04/24.
+//
+
+let baseEndPoint = "https://api.codesquad.kr/onban"
+
+let main = "main"
+let mainHeader = "온 가족이 좋아하는 든든한 메인 요리"
+let mainDictionary = [main : mainHeader]
+
+let soup = "soup"
+let soupHeader = "정성과 건강이 가득 담긴 국물 요리"
+let soupDictionary = [soup : soupHeader]
+
+let side = "side"
+let sideHeader = "식탁을 풍성하게 하는 정갈한 밑반찬"
+let sideDictionary = [side : sideHeader]
+
+let headerMessages = [mainDictionary, soupDictionary, sideDictionary]
--- Onban/OnbanUrl.swift
+++
@@ -1,8 +0,0 @@
-//
-//  OnbanUrl.swift
-//  Onban
-//
-//  Created by ilim on 2023/04/24.
-//
-
-let baseEndPoint = "https://api.codesquad.kr/onban"
